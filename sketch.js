376aa22dc49a882fd2c22c5d318f796b0667dece
thismarvin <bend_@outlook.com>
2019-10-10 20:18:22 -0500

    added smart resize logic

--- sketch.js
+++ sketch.js
@@ -1,5 +1,23 @@
+
+function smartResize(paddingPerctange=0.1) {
+    let usable = 1 - paddingPerctange;
+    let finalWidth = windowWidth * usable;
+    let finalHeight = finalWidth * 9 / 16;
+
+    if (finalHeight > windowHeight * usable) {        
+        finalHeight = windowHeight * usable;
+        finalWidth = finalHeight * 16 / 9;
+    }
+    
+    createCanvas(finalWidth, finalHeight);
+}
+
+function windowResized() {
+    smartResize();
+}
+
 function setup() {
-    createCanvas(100, 100);
+    smartResize();
 }
 
 function draw() {
